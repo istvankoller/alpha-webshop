73ab44c49c16f58bb6311ded637ee1337570b377
istvan.koller <istvan.koller@gmail.com>
2022-11-06 17:22:04 +0100

    implement-search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss'],
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
   editedProduct = {};
-  products$;
+  products: any;
+  filteredProducts: any;
+  subscription: Subscription;
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService
+      .getAll()
+      .subscribe(
+        (products) => (this.filteredProducts = this.products = products)
+      );
   }
 
   filter(query: any) {
-    console.log(query.target.value);
+    let searchParam = query.target.value;
+    this.filteredProducts = searchParam
+      ? this.products.filter((p: any) =>
+          p.title.toLowerCase().includes(searchParam.toLowerCase())
+        )
+      : this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {}
